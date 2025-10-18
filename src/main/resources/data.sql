045b7cff55fabf2da4a72948cefac181320b11c4
Xaes <eliasdearmas1@gmail.com>
2025-10-18 20:20:22 -0300

    Dale salame fijate que este todo bien, arreglo de datos en la BD

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
 -- Insertar datos iniciales de películas
 INSERT INTO movies (title, duration_in_minutes, genre, release_date, rating, status) VALUES
-('Inception', 148, 'Sci-Fi', '2010-07-16', 8.8, 'ACTIVE'),
-('The Matrix', 136, 'Sci-Fi', '1999-03-31', 8.7, 'ACTIVE'),
-('Interstellar', 169, 'Sci-Fi', '2014-11-07', 8.6, 'ACTIVE'),
-('The Dark Knight', 152, 'Action', '2008-07-18', 9.0, 'ACTIVE'),
-('Pulp Fiction', 154, 'Crime', '1994-10-14', 8.9, 'ACTIVE');
+('Inception', 148, 'Sci-Fi', '2010-07-16', 8.8, 'AVAILABLE'),
+('The Matrix', 136, 'Sci-Fi', '1999-03-31', 8.7, 'AVAILABLE'),
+('Interstellar', 169, 'Sci-Fi', '2014-11-07', 8.6, 'AVAILABLE'),
+('The Dark Knight', 152, 'Action', '2008-07-18', 9.0, 'AVAILABLE'),
+('Pulp Fiction', 154, 'Crime', '1994-10-14', 8.9, 'AVAILABLE');
